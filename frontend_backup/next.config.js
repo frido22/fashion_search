d1d4fe5481cf52b44870ae26821870f35f4c3820
frido22 <fridolin.haugg@web.de>
2025-04-06 16:25:47 -0400

    Update .gitignore to exclude large directories

--- frontend_backup/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['serpapi.com', 'encrypted-tbn0.gstatic.com', 'encrypted-tbn1.gstatic.com', 'encrypted-tbn2.gstatic.com', 'encrypted-tbn3.gstatic.com'],
-  },
-}
-
-module.exports = nextConfig
